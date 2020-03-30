0273e1c70396a64a2fc3683b8bd0c11d89a8c225
Oscar Osses <oscar.osses@chilecompra.cl>
2020-03-29 21:33:01 -0300

    Clase 192. Validación a nivel de Field (isRequired)

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { reduxForm , Field } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
 // import { connect } from 'react-redux';
 import { setPropsAsInitial } from '../helpers/setPropsAsInitial';
+
+const isRequired = value => (
+   !value && "Este campo es requerido"
+);
+
+const MyField = ({ input, meta }) => (
+   <div>
+      <input {...input} type="text" />
+      {
+         meta.touched && meta.error && <span>{meta.error}</span>
+      }
+   </div>
+);
 
 const CustomerEdit = ({ name, dni, age }) => {
    return (
@@ -11,26 +24,26 @@ const CustomerEdit = ({ name, dni, age }) => {
          <form action="">
             <div>
                <label htmlFor="name">Nombre</label>
-               <Field name="name" component="input" type="text"></Field>
+               <Field name="name" component={MyField} type="text" validate={isRequired}></Field>
             </div>
             <div>
                <label htmlFor="dni">DNI</label>
-               <Field name="dni" component="input" type="text"></Field>
+               <Field name="dni" component={MyField} type="text" validate={isRequired}></Field>
             </div>
             <div>
                <label htmlFor="age">Edad</label>
-               <Field name="age" component="input" type="number"></Field>
+               <Field name="age" component={MyField} type="number" validate={isRequired}></Field>
             </div>
          </form>
       </div>
    )
-}
+};
 
 CustomerEdit.propTypes = {
    name: PropTypes.string,
-   dni: PropTypes.string,                       
+   dni: PropTypes.string,
    age: PropTypes.number,
-}
+};
 
 const customerEditForm = reduxForm({ form: 'CustomerEdit' })(CustomerEdit);
 
